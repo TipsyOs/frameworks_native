0f4571671b40ccd9617ca6e0e5ec21c8bc64ffa9
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-11-23 00:31:01 +0530

    exynos: Fix HDMI include path for exynos

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -137,8 +137,8 @@ ifeq ($(TARGET_USES_QCOM_BSP), true)
 ifeq ($(BOARD_USES_SAMSUNG_HDMI),true)
         LOCAL_CFLAGS += -DSAMSUNG_HDMI_SUPPORT
         LOCAL_SHARED_LIBRARIES += libTVOut libhdmiclient
-        LOCAL_C_INCLUDES += hardware/samsung/$(TARGET_BOARD_PLATFORM)/libhdmi/libhdmiservice
-        LOCAL_C_INCLUDES += hardware/samsung/$(TARGET_BOARD_PLATFORM)/include
+        LOCAL_C_INCLUDES += hardware/samsung/$(TARGET_BOARD_PLATFORM)/hal/libhdmi/libhdmiservice
+        LOCAL_C_INCLUDES += hardware/samsung/$(TARGET_BOARD_PLATFORM)/hal/include
 endif
 
 LOCAL_MODULE := libsurfaceflinger
