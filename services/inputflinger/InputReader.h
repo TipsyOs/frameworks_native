bb3e39ef6d260482c6db74e8058198776b3d0814
nadlabak <pavel@doshaska.net>
2014-12-21 00:10:54 +0200

    Forward port 'Swap volume buttons' (2/3)
    [mikeioannina]: Adjust for 5.0 changes
    
    Change-Id: I5ed4ae2b7e69e2ada067ed1d3524b3d3fad30e2e
    
    Conflicts:
    	services/inputflinger/InputReader.h

--- services/inputflinger/InputReader.h
+++ services/inputflinger/InputReader.h
@@ -144,6 +144,9 @@ struct InputReaderConfiguration {
         // The presence of an external stylus has changed.
         CHANGE_EXTERNAL_STYLUS_PRESENCE = 1 << 7,
 
+        // Volume keys rotation option changed.
+        CHANGE_VOLUME_KEYS_ROTATION = 1 << 7,
+
         // All devices must be reopened.
         CHANGE_MUST_REOPEN = 1 << 31,
     };
@@ -231,6 +234,10 @@ struct InputReaderConfiguration {
     // True to show the location of touches on the touch screen as spots.
     bool showTouches;
 
+    // Remap volume keys according to display rotation
+    // 0 - disabled, 1 - phone or hybrid rotation mode, 2 - tablet rotation mode
+    int volumeKeysRotationMode;
+
     // Ignore finger touches this long after the stylus has been used (including hover)
     nsecs_t stylusPalmRejectionTime;
 
@@ -251,6 +258,7 @@ struct InputReaderConfiguration {
             pointerGestureMovementSpeedRatio(0.8f),
             pointerGestureZoomSpeedRatio(0.3f),
             showTouches(false),
+            volumeKeysRotationMode(0),
             stylusPalmRejectionTime(50 * 10000000LL) // 50 ms
             { }
 
@@ -1139,7 +1147,8 @@ private:
     uint32_t mSource;
     int32_t mKeyboardType;
 
-    int32_t mOrientation; // orientation for dpad keys
+    int32_t mRotationMapOffset; // determines if and how volume keys rotate
+    int32_t mOrientation; // orientation for dpad and volume keys
 
     Vector<KeyDown> mKeyDowns; // keys that are down
     int32_t mMetaState;
